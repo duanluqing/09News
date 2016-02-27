8b379ae5745277c37f8b470e2b3777cdd2b2ab4a
duanluqing <746562153@qq.com>
2016-02-27 19:40:39 +0800

    新建项目11111

---
+++ 2016 News/2016 NewsTests/_016_NewsTests.m
@@ -0,0 +1,39 @@
+//
+//  _016_NewsTests.m
+//  2016 NewsTests
+//
+//  Created by mac on 16/2/27.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _016_NewsTests : XCTestCase
+
+@end
+
+@implementation _016_NewsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
